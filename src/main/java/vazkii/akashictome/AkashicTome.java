a5cb81abf35e70d497bd977ed02f736603c27a2c
Ethryan <3237986+Ethryan@users.noreply.github.com>
2024-06-26 22:20:58 +0200

    fixes

--- src/main/java/vazkii/akashictome/AkashicTome.java
+++ src/main/java/vazkii/akashictome/AkashicTome.java
@@ -16,8 +16,7 @@ public class AkashicTome {
 
     public static final String MOD_ID = "akashictome";
     public static final String MOD_NAME = "Akashic Tome";
-    public static final String BUILD = "GRADLE:BUILD";
-    public static final String VERSION = "GRADLE:VERSION-" + BUILD;
+    public static final String VERSION = "1.0.0";
     public static final String DEPENDENCIES = "required-after:NotEnoughItems";
     public static final String GUI_FACTORY = "vazkii.akashictome.client.GuiFactory";
 
